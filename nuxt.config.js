0515fabed03e3ba71cb06b2a9f2434df6fbe129a
cuican2020g <nsyk46@gmail.com>
2021-01-20 11:53:52 +0800

    test 测试问题

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    publicPath: '/page/',
+    publicPath: '/',
     transpile: [/^element-ui/],
   },
   generate: {
